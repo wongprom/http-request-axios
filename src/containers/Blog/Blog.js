6fb820a6c675ad15181d1708be5afb1e78ef5abf
wongprom <jimmybackstrom@hotmail.com>
2019-11-03 16:37:23 +0100

    Made get request to jsonplaceholderwith axios.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,9 +4,18 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+    //GET request 
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                console.log(response)
+            });
+
+    }
+    render() {
         return (
             <div>
                 <section className="Posts">
