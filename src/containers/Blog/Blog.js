5b25f34c12af177614b624dd3925022c0614e1a5
wongprom <jimmybackstrom@hotmail.com>
2019-11-04 18:39:27 +0100

    Make post selectable with onClick

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostID: null
     }
     //GET request 
     componentDidMount() {
@@ -27,9 +28,17 @@ class Blog extends Component {
             });
 
     }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostID: id })
+    }
     render() {
         let posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
@@ -37,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostID} />
                 </section>
                 <section>
                     <NewPost />
