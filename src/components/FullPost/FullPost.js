218ec7edbf027b13f7644b3e0726b75397ca4883
wongprom <jimmybackstrom@hotmail.com>
2019-11-04 20:50:07 +0100

    Deleted post with axios.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -16,6 +16,13 @@ class FullPost extends Component {
             }
         }
     }
+    deletePostHandler = () => {
+        axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     render() {
         // if props.id === null, = false. Show >Please select a Post!<
         let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
@@ -30,7 +37,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
             );
