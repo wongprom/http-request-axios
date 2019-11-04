dafefe90265d2aa13da3840f875b03ea9be0b151
wongprom <jimmybackstrom@hotmail.com>
2019-11-04 15:19:49 +0100

    Manipulate data. Only output 4 cards instead of 100. Added new property, author, to the posts.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,8 +14,15 @@ class Blog extends Component {
     componentDidMount() {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: "Jimmy"
+                    }
+                })
                 // Add data to our state
-                this.setState({ posts: response.data })
+                this.setState({ posts: updatedPosts })
 
             });
 
