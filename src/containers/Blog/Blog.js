25463fa18b8701a673edd7cf26d0cdad131a26b0
wongprom <jimmybackstrom@hotmail.com>
2019-11-04 21:07:18 +0100

    Handeling error with axios catch.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostID: null
+        selectedPostID: null,
+        error: false,
     }
     //GET request 
     componentDidMount() {
@@ -25,6 +26,10 @@ class Blog extends Component {
                 // Add data to our state
                 this.setState({ posts: updatedPosts })
 
+            })
+            .catch(error => {
+                // console.log(error)
+                this.setState({ error: true })
             });
 
     }
@@ -33,13 +38,17 @@ class Blog extends Component {
         this.setState({ selectedPostID: id })
     }
     render() {
-        let posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />
-        })
+        let posts = <p style={{ textAlign: "center" }}>Something went wrong!!!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
