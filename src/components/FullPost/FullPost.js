03e340af4b7d68680f4a69e68cf80019ff0f0781
wongprom <jimmybackstrom@hotmail.com>
2019-11-04 19:22:20 +0100

    Use componentDidUpdate() to get 1 post with axios with some if() conditions.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
-    render() {
-
-        let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-        // if props.id === null, = false
+    state = {
+        loadedPost: null,
+    }
+    componentDidUpdate() {
         if (this.props.id) {
+            if (!this.state.loadedPost || this.state.loadedPost && this.state.loadedPost.id !== this.props.id) {
+                axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+                    .then(response => {
+                        this.setState({ loadedPost: response.data })
+                    })
+            }
+        }
+    }
+    render() {
+        // if props.id === null, = false. Show >Please select a Post!<
+        let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+        // if props.id === true, = Show >Loading...<
+        if (this.props.id) {
+            post = <p style={{ textAlign: "center" }}>Loading...</p>;
+        }
+        //When the this.setState({ loadedPost: response.data }) is done, show post
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
                 </div>
-
             );
         }
-
         return post;
     }
 }
