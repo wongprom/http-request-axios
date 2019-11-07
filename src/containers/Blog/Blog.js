e1602e99e9a991a9b33fd08ef80a8ee74e4d015c
wongprom <jimmybackstrom@hotmail.com>
2019-11-07 16:40:53 +0100

    Setting up for using react-router-dom
    Installed react-router and react-router-dom. Wrapped our app with <BrowserRouter>. Restrocture folders.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 import axios from '../../axios';
 
@@ -62,12 +60,12 @@ class Blog extends Component {
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
+                {/* <section>
                     <FullPost id={this.state.selectedPostID} />
                 </section>
                 <section>
                     <NewPost />
-                </section>
+                </section> */}
             </div>
         );
     }
