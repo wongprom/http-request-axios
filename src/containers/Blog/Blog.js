3956a497756601d6d6eeae3169b6d0cb731aaa33
wongprom <jimmybackstrom@hotmail.com>
2019-11-07 23:03:15 +0100

    Styled the active NavLinks different ways.
    Hade to change from <Link> to <NavLink>. Can style the active class in css. Use activeClassName attribute in <NavLink>. Inline style by using activeStyle attribute in <NavLink>.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import NewPost from './NewPost/NewPost'
 import Posts from "../Blog/Posts/Posts";
@@ -13,12 +13,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: "orange",
+                                    textDecoration: "underline"
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: "#submit",
                                 search: "?quick-submit=true"
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
