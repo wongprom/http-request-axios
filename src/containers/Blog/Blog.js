ec9736f0af43974911e4b0560a3a5a186bc91768
wongprom <jimmybackstrom@hotmail.com>
2019-11-07 21:33:23 +0100

    Renderd a component within <Route> instead of <H1>

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,13 +12,14 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li> <a href="/">Home</a></li>
+                            <li><a href="/">Home</a></li>
                             <li><a href="/new-post">New Post</a></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                {/* <Posts /> */}
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+
             </div>
         );
     }
