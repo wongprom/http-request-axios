85a69d2feab198a3602596daca5d7baaaacd7035
wongprom <jimmybackstrom@hotmail.com>
2019-11-08 12:55:15 +0100

    Using Switch from  react-router-dom to just render one component at the time. To use this method it is important to have the <Route> in the corrrect order.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import NewPost from './NewPost/NewPost';
 import Posts from "../Blog/Posts/Posts";
@@ -32,9 +32,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
